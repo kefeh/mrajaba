1e7053ca75eec36013a187a84d82220aade19970
kefeh <kefehcollins@gmail.com>
2020-08-30 11:50:29 +0100

    adds the search component and its styling

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../Stylesheets/Assets.css'
+import '../Stylesheets/Search.css'
 import SearchIcon from '@material-ui/icons/Search';
 
 
 function Search() {
     return (
         <div className="search">
-            <form action="">
-                <SearchIcon/>
-                <input type="text"/>
+            <form  className="search__form" action="">
+                <SearchIcon className="icon search__form-icon"/>
+                <input className="search__form-input" type="text"/>
             </form>
         </div>
     )
