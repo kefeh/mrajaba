9b125d4b1bc5a81b82db54147051986d9b6f841a
kefeh <kefehcollins@gmail.com>
2020-08-31 06:48:12 +0100

    adds the main component and its styling

---
+++ frontend/src/Components/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Stylesheets/MainContent.css'
+import SideBar from './SideBar'
+
+function MainContent() {
+    return (
+        <div className="main-content">
+            <SideBar />
+            <div className="main-content__b">
+                
+            </div>
+        </div>
+    )
+}
+
+export default MainContent
