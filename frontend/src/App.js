b395c4b64d7377516d8099aa291c45fb2d3d51f9
kefeh <kefehcollins@gmail.com>
2020-09-01 12:31:54 +0100

    adds the register component and its neccessary stules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import TestPage from './Components/TestPage';
-import Search from './Components/Search';
-import File from './Components/File';
+import Register from './Components/Register';
 import News from './Components/News';
 import MainContent from './Components/MainContent';
 
 function App() {
   return (
     <div className="App">
-      <News />
-      <MainContent />
+      {/* <News />
+      <MainContent /> */}
+      <Register/>
     </div>
   );
 }
---
+++ frontend/src/Components/Register.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import '../Stylesheets/Assets.css'
+import '../Stylesheets/Register.css'
+
+function Register() {
+    return (
+        <div className="register">
+            <div className="register__image">
+                <img src="../images/auth.png" alt=""/>
+            </div>
+            <form action="" className="register__form">
+                <div className="form-item register__form-name">
+                    <label>Name</label>
+                    <input type="text"/>
+                </div>
+
+                <div className="form-item register__form-email">
+                    <label>Email</label>
+                    <input type="text"/>
+                </div>
+                
+                <div className="form-item register__form-password">
+                    <label>Password</label>
+                    <input type="password"/>
+                </div>
+
+                <div className="form-item register__form-confirm-password">
+                    <label>Confirm Password</label>
+                    <input type="password"/>
+                </div>
+                <div className="checkbox">
+                    <input type="checkbox"/>
+                    <label>Admin</label>
+                </div>
+
+                <div className="register__buttons">
+                    <div className='btn btn__outline'>
+                        <span>Cancel</span>
+                    </div>
+                    <div className='btn btn__primary'>
+                        <span>save</span>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Register
