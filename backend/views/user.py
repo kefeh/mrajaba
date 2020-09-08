2740b29223ae14a6487789a3f878b33e02123ef1
kefeh <kefehcollins@gmail.com>
2020-09-08 05:55:34 +0100

    adds the views that handles the login utility

--- backend/views/user.py
+++ backend/views/user.py
@@ -1,4 +1,4 @@
-from utils.users import signup_user_util
+from utils.users import signup_user_util, login_user_util
 
 def register_user(request):
     signup_resp = signup_user_util(request)
@@ -7,3 +7,12 @@ def register_user(request):
     else:
         signup_resp['success'] = True
         return signup_resp
+
+
+def login_user(request):
+    login_resp = login_user_util(request)
+    if "error" in login_resp:
+        login_resp['success'] = False
+    else:
+        login_resp['success'] = True
+        return login_resp
