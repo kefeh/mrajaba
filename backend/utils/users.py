e8dacbca5dd01c7264d2e97486f581ee8ab691c1
kefeh <kefehcollins@gmail.com>
2020-09-08 14:19:47 +0100

    making some changes to the backend

--- backend/utils/users.py
+++ backend/utils/users.py
@@ -7,43 +7,40 @@ from models import users
 from requests.exceptions import HTTPError
 
 def signup_user_util(request):
-    # email = request.json['email']
-    email = "kk@gmail.com"
-    # password = request.json['password']
-    password = "stryssss"
-    name = "requ"
-    # name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+    name = request.json['name']
     created_at = datetime.now().strftime('%Y-%m-%d %H:%M')
 
     # create new authenticated user
-    # try:
-    user = auth.create_user_with_email_and_password(
-        email, password)
-    user_id = str(datetime.timestamp(datetime.now()))
-    jwt = user['idToken']
-    users.document(user_id).set({
-        "name": name,
-        "email": email,
-        "created_at": created_at
-    })
-    return {
-        "user": {
-            "id": user_id,
+    try:
+        user = auth.create_user_with_email_and_password(
+            email, password)
+        user_id = str(datetime.timestamp(datetime.now()))
+        jwt = user['idToken']
+        users.document(user_id).set({
             "name": name,
             "email": email,
-            "token": jwt
+            "created_at": created_at
+        })
+        return {
+            "user": {
+                "id": user_id,
+                "name": name,
+                "email": email,
+                "token": jwt
+            }
         }
-    }
-    # except HTTPError as e:
-    #     print(e)
-    #     exception = str(e.__repr__()).split('"message":')[1]
-    #     exception = exception.split(',')[0]
-    #     return{'error': f"unable to signup user {exception}",
-    #             'status_code': 400}
-    # except Exception as ex:
-    #     print(ex)
-    #     return{'error': f"unable to signup user SEVER ERROR",
-    #             'status_code': 490}
+    except HTTPError as e:
+        print(e)
+        exception = str(e.__repr__()).split('"message":')[1]
+        exception = exception.split(',')[0]
+        return{'error': f"unable to signup user {exception}",
+                'status_code': 400}
+    except Exception as ex:
+        print(ex)
+        return{'error': f"unable to signup user SEVER ERROR",
+                'status_code': 490}
 
 
 def login_user_util(request):
