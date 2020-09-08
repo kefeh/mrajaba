5df951bd0bbca2b8cc1088ae7688f4e938348745
kefeh <kefehcollins@gmail.com>
2020-09-08 05:42:34 +0100

    adds the view functions for the login and the signup

--- backend/views/user.py
+++ backend/views/user.py
@@ -1,4 +1,9 @@
 from utils.users import signup_user_util
 
 def register_user(request):
-    signup_user_util(request)
+    signup_resp = signup_user_util(request)
+    if "error" in signup_resp:
+        signup_resp['success'] = False
+    else:
+        signup_resp['success'] = True
+        return signup_resp
