d757e7e2ab1404a4a64d5d42bccc5a3cfbb80a65
kefeh <kefehcollins@gmail.com>
2020-09-08 15:39:52 +0100

    adds routing to the application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,34 @@ import './App.css';
 import MainContent from './Components/MainContent';
 import News from './Components/News';
 import Register from './Components/Register';
+import Login from './Components/Login';
 import { useStateValue } from './Data/StateProvider';
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 function App() {
   const [{showRegister}] = useStateValue();
   return (
     <div className="App">
-      { typeof(showRegister) === 'undefined' || showRegister === false ?  
-      ( <>
-        <News />
-        <MainContent /> 
-      </> ):
-      <Register/>}
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            { typeof(showRegister) === 'undefined' || showRegister === false ?  
+            ( <>
+              <News />
+              <MainContent /> 
+            </> ):
+            <Register/>}
+          </Route>
+          <Route path="/login" component={Login} />
+          {/* <Route path="/logout" component={Logout} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
