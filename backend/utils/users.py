ccec71c2b8f4f3be1c685d150fdea709a6b8c7ba
kefeh <kefehcollins@gmail.com>
2020-09-08 05:54:53 +0100

    adds the utility function for the login

--- backend/utils/users.py
+++ backend/utils/users.py
@@ -11,5 +11,42 @@ def signup_user_util(request):
     created_at = datetime.now().strftime('%Y-%m-%d %H:%M')
 
     # create new authenticated user
-    user = auth.create_user_with_email_and_password(email, password)
-    print(user)
+    try:
+        user = auth.create_user_with_email_and_password(
+            email, password)
+        user_id = datetime.timestamp(datetime.now())
+        jwt = user['idToken']
+        users.document(user_id).set({
+            "name": name,
+            "email": email,
+            "created_at": created_at
+        })
+        return {
+            "user": {
+                "id": user_id,
+                "name": name,
+                "email": email,
+                "token": jwt
+            }
+        }
+    except Exception as e:
+        return{'error': f"unable to signup user {e}"}
+
+
+def login_user_util(request):
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    try:
+        user = auth.sign_in_with_email_and_password(
+            email, password)
+        user_info = users.document().get('email' == email)
+        jwt = user['idToken']
+        user_info = {**user_info, "token": jwt}
+
+        return {
+            "user" : user_info
+        }
+    except Exception as e:
+        return {"error": f"Unale to signin user {e}"}
+
