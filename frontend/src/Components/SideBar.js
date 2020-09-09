43b929a6ea9c9efc293f8ae5832106db50472b04
kefeh <kefehcollins@gmail.com>
2020-09-09 10:52:55 +0100

    adds the dynamic fetch and populate of the information in the sidebar

--- frontend/src/Components/SideBar.js
+++ frontend/src/Components/SideBar.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
+import $ from 'jquery';
 import '../Stylesheets/SideBar.css'
 import AddIcon from '@material-ui/icons/Add';
 import GroupIcon from '@material-ui/icons/Group';
 import { useStateValue } from '../Data/StateProvider';
 
 function SideBar() {
-    const [{}, dispatch] = useStateValue();
+    const [{active_user, user}, dispatch] = useStateValue();
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+    
+    const getUsers = () => {
+        $.ajax({
+            url: '/users', //TODO: update request URL
+            type: "GET",
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({}),
+            xhrFields: {
+              withCredentials: true
+            },
+            crossDomain: true,
+            success: (result) => {
+              console.log(result.users)
+              setUsers(result.users)
+              if(result.users.length > 0) {
+                  setActive(result.users[0].email)
+              }
+              return;
+            },
+            error: (error) => {
+              // console.log(error)
+              // alert(error.responseJSON.message)
+              this.setState({
+                fetchingInProgress: false,
+              })
+              return;
+            }
+          })
+    }
+
+    const setActive = (email) => {
+        dispatch({
+            type: 'ADD_ACTIVE_USER',
+            item: email,
+        })
+    } 
 
     const toggleShowRegister = () => {
         dispatch({
@@ -20,58 +64,22 @@ function SideBar() {
                 TEAMS
             </div>
             <div className="sidebar__list">
-               <div className="sidebar__list-item sidebar__active">
+        {users.length > 0 && users.map((item, ind) => (
+            
+               <div key={ind} onClick={() => setActive(item.email)} className={`sidebar__list-item ${active_user === item.email ? 'sidebar__active': ''}`}>
                     <div className="sidebar__list-item-icon">
                         <GroupIcon />
                     </div>
                     <div className="sidebar__list-item-text">
-                        Central
+                        {item.name}
                     </div>
                </div>
-               <div className="sidebar__list-item">
-                    <div className="sidebar__list-item-icon">
-                        <GroupIcon />
-                    </div>
-                    <div className="sidebar__list-item-text">
-                    Centre
-                    </div>
-               </div>
-               <div className="sidebar__list-item">
-                    <div className="sidebar__list-item-icon">
-                        <GroupIcon />
-                    </div>
-                    <div className="sidebar__list-item-text">
-                    South West
-                    </div>
-               </div>
-               <div className="sidebar__list-item">
-                    <div className="sidebar__list-item-icon">
-                        <GroupIcon />
-                    </div>
-                    <div className="sidebar__list-item-text">
-                    West
-                    </div>
-               </div>
-               <div className="sidebar__list-item">
-                    <div className="sidebar__list-item-icon">
-                        <GroupIcon />
-                    </div>
-                    <div className="sidebar__list-item-text">
-                    East
-                    </div>
-               </div>
-               <div className="sidebar__list-item">
-                    <div className="sidebar__list-item-icon">
-                        <GroupIcon />
-                    </div>
-                    <div className="sidebar__list-item-text">
-                    Far North
-                    </div>
-               </div>
+               
+        )) }
             </div>
             <div onClick={toggleShowRegister} className="btn btn__icon-btn sidebar__btn">
-                    <AddIcon className="icon"/>
-                </div>
+                <AddIcon className="icon"/>
+            </div>
         </div>
     )
 }
