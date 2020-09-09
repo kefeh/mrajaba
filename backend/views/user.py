25e42b4fa6538a6bec105dd5586100f3eef04e48
kefeh <kefehcollins@gmail.com>
2020-09-09 10:51:06 +0100

    adds the view function that handles the fetch all users

--- backend/views/user.py
+++ backend/views/user.py
@@ -1,4 +1,4 @@
-from utils.users import signup_user_util, login_user_util
+from utils.users import signup_user_util, login_user_util, get_all_users_util
 
 def register_user(request):
     signup_resp = signup_user_util(request)
@@ -18,3 +18,12 @@ def login_user(request):
         login_resp['success'] = True
         login_resp['status_code'] = 200
     return login_resp
+
+def get_all_users():
+    users_data = get_all_users_util()
+    if "error" in users_data:
+        users_data['success'] = False
+    else:
+        users_data['success'] = True
+        users_data['status_code'] = 200
+    return users_data
