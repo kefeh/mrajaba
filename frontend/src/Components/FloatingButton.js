9b43ffa774c9f701549f2ab78a23a9ac5a532cbd
kefeh <kefehcollins@gmail.com>
2020-09-11 13:05:17 +0100

    reducing the number of floating buttons based on need

--- frontend/src/Components/FloatingButton.js
+++ frontend/src/Components/FloatingButton.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import '../Stylesheets/FloatingButton.css';
 import AddIcon from '@material-ui/icons/Add';
+import { useStateValue } from '../Data/StateProvider'
 
 function FloatingButton() {
+    const [{showAddFolder}, dispatch] = useStateValue();
+
+    const toggleShowAddFolder = () => {
+        dispatch({
+            type: 'SHOW_ADD_FOLDER',
+            item: true
+        })
+    }
+
     return (
         <div className="floating-action" >
             <div className="floating-action__side" >
-                <div className='btn btn__outline'>
+                <div onClick={toggleShowAddFolder} className='btn btn__outline'>
                     <span>Folder</span>
                 </div>
                 <div className='btn btn__outline'>
-                    <span>Document</span>
-                </div>
-                <div className='btn btn__outline'>
-                    <span>Video</span>
-                </div>
-                <div className='btn btn__outline'>
-                    <span>Image</span>
+                    <span>File</span>
                 </div>
             </div>
             <div className="btn__primary btn__floating-icon-btn ">
