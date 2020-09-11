ed07603ac718f3fb7d4452f60bac4509a1ed83bc
kefeh <kefehcollins@gmail.com>
2020-09-11 13:30:07 +0100

    adds constraint to show add floating button not for the recently ta

--- frontend/src/Components/ContentSection.js
+++ frontend/src/Components/ContentSection.js
@@ -2,8 +2,11 @@ import React from 'react'
 import '../Stylesheets/ContentSection.css'
 import FloatingButton from './FloatingButton'
 import File from './File'
+import { useStateValue } from '../Data/StateProvider';
+
 
 function ContentSection() {
+    const [{active_nav}] = useStateValue();
     return (
         <div className="content-section">
             <div className="content-section__main">
@@ -18,9 +21,11 @@ function ContentSection() {
                 <File />
                 <File />
             </div>
+            {active_nav!== "Recently" && (
             <div className="content-section__floating-button">
                 <FloatingButton />
             </div>
+            )}
         </div>
     )
 }
