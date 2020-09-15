eadf3ab845ab8a70d1cf88987a349d568b2883a7
kefeh <kefehcollins@gmail.com>
2020-09-15 12:43:03 +0100

    adds the view function for gettinng all folders

--- backend/views/folder.py
+++ backend/views/folder.py
@@ -1,4 +1,4 @@
-from utils.folder import add_folder_util
+from utils.folder import add_folder_util, get_folders_util
 
 def add_folder(request):
     add_folder_resp = add_folder_util(request)
@@ -10,3 +10,13 @@ def add_folder(request):
         add_folder_resp['status_code'] = 200
 
     return add_folder_resp
+
+def get_folders_view(request):
+    folder_data = get_folders_util(request)
+    if "error" in folder_data:
+        folder_data['success'] = False
+    else:
+        folder_data['success'] = True
+        folder_data['status_code'] = 200
+    return folder_data
+
