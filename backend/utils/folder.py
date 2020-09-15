828e1131c94f3079e2db3777d48bbc8e7ab635c3
kefeh <kefehcollins@gmail.com>
2020-09-15 12:42:40 +0100

    adds the utility function for gettinng all folders

--- backend/utils/folder.py
+++ backend/utils/folder.py
@@ -22,3 +22,23 @@ def add_folder_util(request):
     except Exception as e:
         return {'error': f"unable to create folder {e}",
                 'status_code': 400}
+
+def get_folders_util(request):
+    user = request['user']
+    clas = request['class']
+    category = request['category']
+    try:
+        file_list = folders.where(
+            'user', '==', user).where('class', '==', clas).where(
+                'category', '==', category).get()
+
+        new_file_list = [a_file.to_dict() for a_file in file_list]
+        return {
+                'folders': new_file_list
+            }
+    except Exception as ex:
+        import traceback
+        traceback.print_exc()
+        print(ex)
+        return{'error': f"unable to get folders SEVER ERROR",
+                'status_code': 490}
