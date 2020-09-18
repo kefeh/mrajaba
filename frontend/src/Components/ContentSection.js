a5e56c5f68f264a417e59c3b4fc7c2e749d78dad
kefeh <kefehcollins@gmail.com>
2020-09-18 10:25:04 +0100

    adds the code that distinguishly displays files and folders

--- frontend/src/Components/ContentSection.js
+++ frontend/src/Components/ContentSection.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../Stylesheets/ContentSection.css'
 import FloatingButton from './FloatingButton'
 import File from './File'
+import Folder from './Folder'
+
 import { useStateValue } from '../Data/StateProvider';
 
 
-function ContentSection() {
+function ContentSection({files, folders}) {
     const [{active_nav}] = useStateValue();
+
     return (
         <div className="content-section">
             <div className="content-section__main">
-                <File />
-                <File />
-                <File />
-                <File />
-                <File />
-                <File />
-                <File />
-                <File />
-                <File />
-                <File />
+                {folders.length !== 0 && folders.map((folder, index) => (
+                    <Folder key={index} folder={folder}/>
+                ))} 
+                {files.length !== 0 && files.map((file, index) => (
+                    <File key={index} file={file}/>
+                ))}
             </div>
             {active_nav!== "Recently" && (
             <div className="content-section__floating-button">
