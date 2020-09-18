6079d6e844bf5a574fa298f6aae7ac803798e482
kefeh <kefehcollins@gmail.com>
2020-09-18 10:26:28 +0100

    adds the code for using dinamic names for folders

--- frontend/src/Components/Folder.js
+++ frontend/src/Components/Folder.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../Stylesheets/Folder.css'
 
-function Folder() {
+function Folder({folder}) {
     return (
         <div className="folder">
             <div className="folder__content">
                 <img src="../images/folder.png" alt="folder" />
             </div>
             <div className="folder__title">
-            The gem might not be able to compile, to solve this you might need to
+            {folder.folder}
             </div>
         </div>
     )
