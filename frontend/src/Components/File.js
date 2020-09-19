a6c7ecfb91cda0ae54aebcf1525a8c4bc68ba1c4
kefeh <kefehcollins@gmail.com>
2020-09-19 12:02:09 +0100

    adds changes for variable file icons depending on type

--- frontend/src/Components/File.js
+++ frontend/src/Components/File.js
@@ -3,15 +3,22 @@ import '../Stylesheets/Assets.css'
 import '../Stylesheets/File.css'
 import GetAppIcon from '@material-ui/icons/GetApp';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import { useStateValue } from '../Data/StateProvider';
 
 function File({file}) {
+    const [{active_nav}] = useStateValue();
+    const icon_path = {
+        "Documents": "../images/file.png",
+        "Videos": "../images/video.png",
+        "Images": "../images/image.png",
+    }
     return (
         <div className="file">
             <div className="file__delete-buttone">
                 <DeleteOutlineIcon />
             </div>
             <div className="file__content">
-                <img src="../images/file.png" alt="file thumbnail"/>
+                <img src={icon_path[active_nav]} alt="file thumbnail"/>
                 <div className="btn btn__icon-btn btn__text-icon-btn">
                     <GetAppIcon className="icon"/>
                     <span>Download</span>
