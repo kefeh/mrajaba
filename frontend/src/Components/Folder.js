324fed807b42681ed3d674f7101e644f64bed173
kefeh <kefehcollins@gmail.com>
2020-09-19 12:04:57 +0100

    adds code for handling folder click

--- frontend/src/Components/Folder.js
+++ frontend/src/Components/Folder.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import '../Stylesheets/Folder.css'
+import { useStateValue } from '../Data/StateProvider';
 
-function Folder({folder}) {
+
+function Folder({a_folder}) {
+    const [{}, dispatch] = useStateValue();
+
+    // useEffect(() => {
+    //     getFiles()
+    // }, [active_user, active_class, active_nav, folder])
+
+    const setFolder =(folder_name) => {
+        console.log(folder_name);
+        dispatch({
+            type: 'ADD_ACTIVE_FOLDER',
+            item: folder_name
+        })
+    }
+
     return (
-        <div className="folder">
+        <div onClick={()=>{setFolder(a_folder.folder)}} className="folder">
             <div className="folder__content">
                 <img src="../images/folder.png" alt="folder" />
             </div>
             <div className="folder__title">
-            {folder.folder}
+            {a_folder.folder}
             </div>
         </div>
     )
