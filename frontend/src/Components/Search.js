e7558cb7a1cedbe452b4b2333146cf5a9fbe3346
kefeh <kefehcollins@gmail.com>
2020-09-21 17:04:08 +0100

    adds searching

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -2,14 +2,24 @@ import React from 'react'
 import '../Stylesheets/Assets.css'
 import '../Stylesheets/Search.css'
 import SearchIcon from '@material-ui/icons/Search';
-
+import { useStateValue } from '../Data/StateProvider';
 
 function Search() {
+    const [{}, dispatch] = useStateValue();
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        dispatch({
+            type: 'SET_SEARCHTERM',
+            item:  event.target.value,
+        })
+    }
+
     return (
         <div className="search">
             <form  className="search__form" action="">
                 <SearchIcon className="icon search__form-icon"/>
-                <input className="search__form-input" type="text"/>
+                <input onChange={handleChange} className="search__form-input" type="text"/>
             </form>
         </div>
     )
