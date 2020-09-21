0bdd8e6593de4ba57427d51640a43e585abcc685
kefeh <kefehcollins@gmail.com>
2020-09-21 17:03:32 +0100

    adds searching constraint to filter new list items

--- frontend/src/Components/ContentSection.js
+++ frontend/src/Components/ContentSection.js
@@ -8,18 +8,33 @@ import { useStateValue } from '../Data/StateProvider';
 
 
 function ContentSection({files, folders, getFiles, getFolders}) {
-    const [{active_nav}] = useStateValue();
-
+    const [{active_nav, search_term}] = useStateValue();
+    const [documents, setDocuments] = useState([]);
+    const [a_files, setFiles] = useState([]);
+    useEffect(() => {
+        if(search_term !== '') {
+            var some_files = files.filter(file =>
+                file.name.toLowerCase().includes(search_term)
+            );
+            setDocuments([])
+            setFiles(some_files)
+        }else {
+            setDocuments(folders)
+            setFiles(files)
+        }
+        console.log(documents)
+        console.log(some_files)
+    }, [search_term])
     return (
         <div className="content-section">
             <div className="content-section__main">
-                {folders.length !== 0 && folders.map((a_folder, index) => (
+                {documents.length !== 0 && documents.map((a_folder, index) => (
                     <Folder  key={index} a_folder={a_folder}/>
                 ))}
-                {files.length !== 0 && files.map((file, index) => (
+                {a_files.length !== 0 && a_files.map((file, index) => (
                     <File key={index} file={file}/>
                 ))}
-                {files.length === 0 && folders.length === 0 && <div>No files uploaded</div>}
+                {a_files.length === 0 && folders.length === 0 && <div>No files uploaded</div>}
             </div>
             {active_nav!== "Recently" && (
             <div className="content-section__floating-button">
