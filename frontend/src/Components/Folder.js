df93c51be70ffd203a4fb3c0c2c8cb62e18254a3
kefeh <kefehcollins@gmail.com>
2020-09-21 17:06:01 +0100

    adds delete folder frontend api fetch and refresh on delete constraint

--- frontend/src/Components/Folder.js
+++ frontend/src/Components/Folder.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Stylesheets/Folder.css'
 import { useStateValue } from '../Data/StateProvider';
-
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import $ from 'jquery';
 
 function Folder({a_folder}) {
-    const [{}, dispatch] = useStateValue();
 
-    // useEffect(() => {
-    //     getFiles()
-    // }, [active_user, active_class, active_nav, folder])
+    const [{active_user, active_class, active_nav, refresh, folder, showAddFile}, dispatch] = useStateValue();
 
+    const [fetchingInProgress, setFetchingInProgress] = useState(false);
+
+    const refresher = () => {
+        dispatch({
+            type: 'SET_REFRESH',
+            item: !(refresh),
+        })
+    }
     const setFolder =(folder_name) => {
         console.log(folder_name);
         dispatch({
@@ -18,9 +24,39 @@ function Folder({a_folder}) {
         })
     }
 
+    const deleteFolder = (id) => {
+        setFetchingInProgress(true);
+        $.ajax({
+            url: `/folders?id=${id}&class=${active_class}&category=${active_nav}&user=${active_user}`, //TODO: update request URL
+            type: "DELETE",
+            dataType: 'json',
+            contentType: 'application/json',
+            xhrFields: {
+              withCredentials: true
+            },
+            crossDomain: true,
+            success: (result) => {
+              console.log(result)
+              setFetchingInProgress(false);
+              refresher();
+              return;
+            },
+            error: (error) => {
+              // console.log(error)
+              // alert(error.responseJSON.message)
+              setFetchingInProgress(false)
+              refresher();
+              return;
+            }
+        })
+    }
+
     return (
-        <div onClick={()=>{setFolder(a_folder.folder)}} className="folder">
-            <div className="folder__content">
+        <div className="folder">
+            <div onClick={()=>{deleteFolder(a_folder.id)}} className="folder__delete-buttone">
+                <DeleteOutlineIcon />
+            </div>
+            <div onClick={()=>{setFolder(a_folder.folder)}} className="folder__content">
                 <img src="../images/folder.png" alt="folder" />
             </div>
             <div className="folder__title">
