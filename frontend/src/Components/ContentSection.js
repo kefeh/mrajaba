af276a886ef3a96643958a1e875a7ebc11906550
kefeh <kefehcollins@gmail.com>
2020-10-07 14:39:22 +0100

    adds files and folders as usestate dependencies and removes unused code

--- frontend/src/Components/ContentSection.js
+++ frontend/src/Components/ContentSection.js
@@ -5,37 +5,31 @@ import File from './File'
 import Folder from './Folder'
 
 import { useStateValue } from '../Data/StateProvider';
-import { useAsyncReference } from './AsyncReference';
 
 
-function ContentSection({files, folders, getFiles, getFolders}) {
-    const asyncFiles = useAsyncReference(files, true);
-    const asyncFolders = useAsyncReference(folders, true);
+function ContentSection({files, folders}) {
     const [{active_nav, search_term}] = useStateValue();
     const [a_files, setFiles] = useState([]);
     useEffect(() => {
-        console.log(asyncFiles.current)
         if(search_term !== '') {
-            var some_files = asyncFiles.current.filter(file =>
+            var some_files = files.filter(file =>
                 file.name.toLowerCase().includes(search_term)
             );
             setFiles(some_files)
-            console.log(some_files)
         }else {
-            setFiles(asyncFiles.current)
-            console.log(asyncFiles.current)
+            setFiles(files)
         }
-    }, [search_term, active_nav])
+    }, [search_term, active_nav, files, folders])
     return (
         <div className="content-section">
             <div className="content-section__main">
-                {search_term === '' && asyncFolders.current.length !== 0 && asyncFolders.current.map((a_folder, index) => (
+                {search_term === '' && folders.length !== 0 && folders.map((a_folder, index) => (
                     <Folder  key={index} a_folder={a_folder}/>
                 ))}
                 {a_files.length !== 0 && a_files.map((file, index) => (
                     <File key={index} file={file}/>
                 ))}
-                {a_files.length === 0 && (asyncFolders.current.length === 0 || search_term !== '') && <div>No files uploaded</div>}
+                {a_files.length === 0 && (folders.length === 0 || search_term !== '') && <div>No files uploaded</div>}
             </div>
             {active_nav!== "Recently" && (
             <div className="content-section__floating-button">
