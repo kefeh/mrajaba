ea4a7cfd4054c9c41f2df1c785ac592049e0d11e
kefeh <kefehcollins@gmail.com>
2020-10-08 10:50:34 +0100

    adds the news models

--- backend/models.py
+++ backend/models.py
@@ -4,4 +4,5 @@ from config import db
 users = db.collection(u'users')
 folders = db.collection(u'folders')
 files = db.collection(u'files')
+news = db.collection(u'news')
 notifications = db.collection(u'notifications')
